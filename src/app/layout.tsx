e88821c1dbba9c756fd3bfd6acb6c4a70f6f8ba1
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-06-13 16:42:35 -0400

    Have body take up full screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={inter.className}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
